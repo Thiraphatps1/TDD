8cd36372a687b41f3dd250abccfe8b061f74fa76
Thiraphat <hone191@gmail.com>
2019-04-25 13:24:34 +0700

    View

--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from .models import Entry
 # Create your tests here.
-
+from django.contrib.auth import get_user_model
+from django_webtest import WebTest
 
 
 
@@ -23,4 +24,41 @@ class ProjectTests(TestCase):
 
     def test_homepage(self):
         response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+class HomePageTests(TestCase):
+
+    """Test whether our blog entries show up on the homepage"""
+
+    def setUp(self):
+        self.user = get_user_model().objects.create(username='some_user')
+
+    def test_one_entry(self):
+        Entry.objects.create(title='1-title', body='1-body', author=self.user)
+        response = self.client.get('/')
+        self.assertContains(response, '1-title')
+        self.assertContains(response, '1-body')
+
+    def test_two_entries(self):
+        Entry.objects.create(title='1-title', body='1-body', author=self.user)
+        Entry.objects.create(title='2-title', body='2-body', author=self.user)
+        response = self.client.get('/')
+        self.assertContains(response, '1-title')
+        self.assertContains(response, '1-body')
+        self.assertContains(response, '2-title')
+
+    def test_no_entries(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'No blog entries yet.')
+
+
+class EntryViewTest(WebTest):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create(username='some_user')
+        self.entry = Entry.objects.create(title='1-title', body='1-body',
+                                          author=self.user)
+
+    def test_basic_view(self):
+        response = self.client.get(self.entry.get_absolute_url())
         self.assertEqual(response.status_code, 200)
--- myblog/urls.py
+++ myblog/urls.py
@@ -2,8 +2,14 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from . import views
+import blog.urls
+
 
 urlpatterns = [
     url(r'^$', views.HomeView.as_view(), name='home'),
+
+    url(r'^', include(blog.urls)),
     url(r'^admin/', admin.site.urls),
 ]
+
+  
