18a3a00fa6a1940fb0798fb89eb0979eddde085a
Thiraphat <hone191@gmail.com>
2019-04-26 09:14:55 +0700

    Custom Template Tags

--- blog/tests.py
+++ blog/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.template import Template, Context
 from django.contrib.auth import get_user_model
 from .forms import CommentForm
 from .models import Entry, Comment
@@ -75,6 +76,7 @@ class EntryViewTest(WebTest):
         page = self.app.get(self.entry.get_absolute_url())
         self.assertEqual(len(page.forms), 1)
 
+    
     def test_form_error(self):
         page = self.app.get(self.entry.get_absolute_url())
         page = page.form.submit()
@@ -130,3 +132,26 @@ class CommentFormTest(TestCase):
             'email': ['This field is required.'],
             'body': ['This field is required.'],
         })
+
+class EntryHistoryTagTest(TestCase):
+
+    TEMPLATE = Template("{% load blog_tags %} {% entry_history %}")
+
+    def setUp(self):
+        self.user = get_user_model().objects.create(username='zoidberg')
+
+    def test_entry_shows_up(self):
+        entry = Entry.objects.create(author=self.user, title="My entry title")
+        rendered = self.TEMPLATE.render(Context({}))
+        self.assertIn(entry.title, rendered)
+
+    def test_no_posts(self):
+        rendered = self.TEMPLATE.render(Context({}))
+        self.assertIn("No recent entries", rendered)
+
+    def test_many_posts(self):
+        for n in range(1, 6):
+            Entry.objects.create(author=self.user, title="Post #{0}".format(n))
+        rendered = self.TEMPLATE.render(Context({}))
+        self.assertIn("Post #5", rendered)
+        self.assertNotIn("Post #6", rendered)
